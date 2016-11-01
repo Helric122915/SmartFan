f9670a54023397b516f81472be5cd2a9e4723df5
Helric122915 <jbb47@zips.uakron.edu>
2016-11-01 09:49:22 -0400

    Create PWM.py
    Controls the operation of the PWM pin of the RPI.

---
+++ PWM.py
@@ -0,0 +1,23 @@
+import wiringpi
+
+def setuppwm():
+  wiringpi.wiringPiSetup()
+  
+def setpwm(pwm):
+  OUTPUT = 2
+  TURN_OFF = 0
+  PIN_TO_PWM = 1 # GPIO 18 on the RPI
+  
+  if pwm == -1:
+    wiringpi.pinMode(PIN_TO_PWM, TURN_OFF)
+    result = True
+  
+  elif pwm >= 0 and pwm <= 2023:
+    wiringpi.pinMode(PIN_TO_PWM, OUTPUT)
+    wiringpi.pwmWrite(PIN_TO_PWM, pwm)
+    result = True
+    
+  else:
+    result = False
+  
+  return result
