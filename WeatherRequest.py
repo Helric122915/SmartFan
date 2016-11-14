3abcb2400527de02eb132cdf58359ff9b8b238d3
helric122915 <helric122915@yahoo.com>
2016-11-14 15:35:26 +0000

    Changed WeatherRequest into a function

--- WeatherRequest.py
+++ WeatherRequest.py
@@ -4,39 +4,38 @@ import time
 
 api_url = "https://api.darksky.net/forecast/%s/%f,%f"
 api_key = "9c1f7351528fc58f40881d5bf2651719"
-lat = 41.07472
-long = -81.52201
 
+def Current(lat = 41.07472, long = -81.52201)
+  query_url = api_url % (api_key, lat, long)
+  r = requests.get(query_url)
 
-query_url = api_url % (api_key, lat, long)
-r = requests.get(query_url)
-if r.status_code != 200:
-	print "Error:", r.status_code
-
-json_weather = r.json()
-
+  if r.status_code != 200:
+    print "Error:", r.status_code
+  else:
+    json_weather = r.json()
+    return json_weather
 #print json_weather
 #print "\n"
 
-currentTime = json_weather['currently']['time']
+#currentTime = json_weather['currently']['time']
 
-currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(currentTime))
+#currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(currentTime))
 
-print currentTime
+#print currentTime
 
-currentTemp = json_weather['currently']['temperature']
+#currentTemp = json_weather['currently']['temperature']
 
-print "The Current Temperature is: "  + str(currentTemp) + " Fahrenheit"
+#print "The Current Temperature is: "  + str(currentTemp) + " Fahrenheit"
 
-currentWeather = json_weather['currently']['icon']
+#currentWeather = json_weather['currently']['icon']
 
-if "cloud" in currentWeather:
-	print "Currently it is: Cloudy"
-elif "rain" in currentWeather:
-	print "Currently it is: Raining"
-else:
-	print "Currently it is: Clear"
+#if "cloud" in currentWeather:
+#	print "Currently it is: Cloudy"
+#elif "rain" in currentWeather:
+#	print "Currently it is: Raining"
+#else:
+#	print "Currently it is: Clear"
 
-nearestStorm = json_weather['currently']['nearestStormDistance']
+#nearestStorm = json_weather['currently']['nearestStormDistance']
 
-print "The nearest Storm is " + str(nearestStorm) + " Kilometer(s) away."
+#print "The nearest Storm is " + str(nearestStorm) + " Kilometer(s) away."
