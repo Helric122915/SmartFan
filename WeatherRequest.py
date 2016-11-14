bbc842ea7102879f60185a9d96ca89fc5b41fac6
helric122915 <helric122915@yahoo.com>
2016-11-14 15:31:40 +0000

    Initial Adding

---
+++ WeatherRequest.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import time
+
+api_url = "https://api.darksky.net/forecast/%s/%f,%f"
+api_key = "9c1f7351528fc58f40881d5bf2651719"
+lat = 41.07472
+long = -81.52201
+
+
+query_url = api_url % (api_key, lat, long)
+r = requests.get(query_url)
+if r.status_code != 200:
+	print "Error:", r.status_code
+
+json_weather = r.json()
+
+#print json_weather
+#print "\n"
+
+currentTime = json_weather['currently']['time']
+
+currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(currentTime))
+
+print currentTime
+
+currentTemp = json_weather['currently']['temperature']
+
+print "The Current Temperature is: "  + str(currentTemp) + " Fahrenheit"
+
+currentWeather = json_weather['currently']['icon']
+
+if "cloud" in currentWeather:
+	print "Currently it is: Cloudy"
+elif "rain" in currentWeather:
+	print "Currently it is: Raining"
+else:
+	print "Currently it is: Clear"
+
+nearestStorm = json_weather['currently']['nearestStormDistance']
+
+print "The nearest Storm is " + str(nearestStorm) + " Kilometer(s) away."
