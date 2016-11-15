5f012c2c5ea829b4f9d925f2c9c3653a65728bfe
helric122915 <helric122915@yahoo.com>
2016-11-15 18:32:54 +0000

    Made Changes to facilitate the Location Functions

--- WeatherRequest.py
+++ WeatherRequest.py
@@ -1,12 +1,25 @@
 import requests
 import json
 import time
+import LocationRequest
 
 api_url = "https://api.darksky.net/forecast/%s/%f,%f"
 api_key = "9c1f7351528fc58f40881d5bf2651719"
 
-def Current(lat = 41.07472, long = -81.52201)
-  query_url = api_url % (api_key, lat, long)
+def CurrentAddress(address):
+  location = LocationRequest.LatLng(address)
+
+  weather = Current(location.lat, location.lng)
+  return weather
+
+def CurrentTempAddress(address):
+  location = LocationRequest.LatLng(address)
+
+  temp = CurrentTemp(location.lat, location.lng)
+  return temp
+
+def Current(lat = 41.07472, lng = -81.52201):
+  query_url = api_url % (api_key, int(float(lat)), int(float(lng)))
   r = requests.get(query_url)
 
   if r.status_code != 200:
@@ -14,6 +27,13 @@ def Current(lat = 41.07472, long = -81.52201)
   else:
     json_weather = r.json()
     return json_weather
+
+def CurrentTemp(lat = 41.07472, long = -81.52201):
+  json_weather = Current(lat, long)
+
+  return json_weather['currently']['temperature']
+
+
 #print json_weather
 #print "\n"
 
