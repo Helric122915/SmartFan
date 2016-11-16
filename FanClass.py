3c07fb3c8046b0bfea8241a238e0abda68ea15a2
helric122915 <helric122915@yahoo.com>
2016-11-16 02:44:08 +0000

    Removed Comments

--- FanClass.py
+++ FanClass.py
@@ -1,15 +1,5 @@
 class FanData(object):
-  #fanSpeed = 0
-  #roomTemp = 0
 
   def __init__(self,fanSpeed,roomTemp):
     self.fanSpeed = fanSpeed
     self.roomTemp = roomTemp
-
-#def make_FanData(fanSpeed, roomTemp):
-#  data = FanData() 
-# 
-#  data.fanSpeed = fanSpeed
-#  data.roomTemp = roomTemp  
-#  
-#  return data
