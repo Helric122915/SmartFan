301411305e049e059c98aed17a4f348c6f097aab
helric122915 <helric122915@yahoo.com>
2016-11-22 15:19:00 +0000

    Added new variables to the FanClass

--- FanClass.py
+++ FanClass.py
@@ -1,5 +1,25 @@
 class FanData(object):
 
   def __init__(self,fanSpeed,roomTemp):
-    self.fanSpeed = fanSpeed
+    # General Variables
     self.roomTemp = roomTemp
+    self.fanOff = fanOff
+    self.direction = direction
+
+    # Manual Mode Variables
+    self.manualModeFanSpeed = fanSpeed
+    
+    # Schedule Mode Variables
+    # What variables do we need to support this mode?
+
+    # One Source Temp Mode Variables
+    self.oneTempLowTemp = oneTempLowTemp
+    self.oneTempLowSpeed = oneTempLowSpeed
+    self.oneTempHighTemp = oneTempHighTemp
+    self.oneTempHighSpeed = oneTempHighSpeed
+
+    # Two Source Temp Mode Variables
+    self.twoTempLowTemp = twoTempLowTemp
+    self.twoTempLowSpeed = twoTempLowSpeed
+    self.twoTempHighTemp = twoTempHighTemp
+    self.twoTempHighSpeed = twoTempHighSpeed
