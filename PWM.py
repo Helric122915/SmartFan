f1cd2fe773f09e967719d68d0d87aa11f1a16c5d
helric122915 <helric122915@yahoo.com>
2016-11-27 22:24:14 +0000

    Changed the frequency of the PWM to be a static 40kHz. Changed the set mode of the PWM to achieve this and set several values of settings.

--- PWM.py
+++ PWM.py
@@ -2,6 +2,11 @@ import wiringpi # Requires installing from the WiringPi/WiringPi-Python wrapper 
 
 def setuppwm():
     wiringpi.wiringPiSetup()
+
+def configurepwm():
+    wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
+    wiringpi.pwmSetClock(10)
+    wiringpi.pwmSetRange(48)
   
 def setpwm(pwm):
     OUTPUT = 2
@@ -12,8 +17,9 @@ def setpwm(pwm):
         wiringpi.pinMode(PIN_TO_PWM, TURN_OFF)
         result = True
   
-    elif pwm >= 0 and pwm <= 2023:
+    elif pwm >= 0 and pwm <= 100:
         wiringpi.pinMode(PIN_TO_PWM, OUTPUT)
+        configurepwm()
         wiringpi.pwmWrite(PIN_TO_PWM, pwm)
         result = True
     
