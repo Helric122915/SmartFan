96886c6a9f7bad43068c684c10157db5a3da96d6
helric122915 <helric122915@yahoo.com>
2016-12-19 16:10:28 +0000

    Added correctional function to the read temperature function.

--- ReadSensor.py
+++ ReadSensor.py
@@ -1,13 +1,19 @@
-import Adafruit_ADS1x15 # Lookup required command to install this
+import Adafruit_ADS1x15 # Google search ADS1015/ADS1115 and follow adafruit tutorial
 
 def readTemp()
     adc = Adafruit_ADS1x15.ADS1015()
     GAIN = 1
-    
+    tempCoef = 19.5    
+    voltAtZero = 400
+
     value = adc.read_adc(0, gain=GAIN)
     
     # Apply corrective function to attain temp value
-    
+    temp = (value - voltAtZero) / tempCoef    
+
+    # Convert from Celsius to Fahrenheit
+    temp = (temp*(9.0/5.0)) + 32
+
     return temp
     
 def readHumidity()
