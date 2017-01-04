532687ce25385a338e76df454cedb1f08d0658f3
Helric122915 <helric122915@yahoo.com>
2017-01-04 15:30:42 +0000

    Updated MainLoop to properly access the webserver

--- MainLoopFunction.py
+++ MainLoopFunction.py
@@ -1,47 +1,60 @@
 import atexit
 import signal
 import PWM
+import FanClass
 #import ScheduleOperation
-#import ManualOperation
+import ManualOperation
 #import OneSourceTemperatureOperation
 #import TwoSourceTemperatureOperation
 import Settings
 import AccessWebServer
 import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) # POWER PUSH BUTTON GPIO PIN 4
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(3, GPIO.LOW) # LED INDICATOR GPIO PIN 3
+
+currentSettings = FanClass.CurrentFanData("Clockwise",-1,True)
+
+def power_callback(channel):
+  global currentSettings
+  if currentSettings.power:
+    currentSettings.power = False
+    GPIO.output(3, GPIO.LOW)
+    print "Power Off"
+  else:
+    currentSettings.power = True
+    GPIO.output(3, GPIO.HIGH)
+    print "Power On"
 
 def exit_handler():
-    print "Turning off the PWM for safety!"
-    PWM.setpwm(-1)
+  print "Turning off the PWM for safety!"
+  PWM.setpwm(-1)
   
 atexit.register(exit_handler)
 
 def handler(signum, frame):
-    print 'Ctrl+Z pressed, but ignored'
+  print 'Ctrl+Z pressed, but ignored'
   
 signal.signal(signal.SIGTSTP, handler)
 
 PWM.setuppwm()
 
-currentPWM = -1
-
-# Get last operation mode used
-    # Access DB or web server to find value
-    # If none exists use default mode (manual?)
-  
-#Settings.ReadSettings(Settings.OperationMode.Manual)
-
-#fanData = AccessWebServer.GetCurrentFanData()
-
-#fanData.roomTemp = 75
-#fanData.fanSpeed = 3700
-
-#AccessWebServer.PostUpdateFanData(fanData)
+manualData = AccessWebServer.GetManual()
 
 while True:
-  fanData = AccessWebServer.GetCurrentFanData()
+  opMode = AccessWebServer.GetOp()
   
-  print "Setting PWM to: " + str(fanData.fanSpeed)
-  PWM.setpwm(fanData.fanSpeed)
+  if opMode == "Manual":
+    manualData = AccessWebServer.GetManual()
+    currentSettings = ManualOperation.AdjustPWM(manualData, currentSettings)
+
+  else:
+    print "Not in Manual"
+    PWM.setpwm(-1)
 
   time.sleep(1)
      
