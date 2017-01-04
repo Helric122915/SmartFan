58e909c8e35b36007949ed7d7fd098647b977721
Helric122915 <helric122915@yahoo.com>
2017-01-04 15:30:59 +0000

    Updated manual operation to use the new manual class

--- ManualOperation.py
+++ ManualOperation.py
@@ -1,3 +1,26 @@
 import FanClass
+import PWM
 
-def AdjustPWM(FanData, CurrentPWM):
+def AdjustPWM(FanData, CurrentSettings):
+  if FanData.direction == "Clockwise" or FanData.direction == "Counterclockwise":
+    if CurrentSettings.direction == FanData.direction:
+      if CurrentSettings.pwm < FanData.pwm:
+        CurrentSettings.pwm = CurrentSettings.pwm + 1
+        PWM.setpwm(CurrentSettings.pwm)
+        print "Setting PWM to " + str(CurrentSettings.pwm) + " " + CurrentSettings.direction
+
+      elif CurrentSettings.pwm > FanData.pwm:
+        CurrentSettings.pwm = CurrentSettings.pwm - 1
+        PWM.setpwm(CurrentSettings.pwm)
+        print "Setting PWM to " + str(CurrentSettings.pwm) + " " + CurrentSettings.direction   
+
+    else:
+       if CurrentSettings.pwm == 0 or CurrentSettings.pwm == -1:
+          CurrentSettings.direction = FanData.direction
+          print "Changing Direction of Fan"
+       else:
+         CurrentSettings.pwm = CurrentSettings.pwm - 1
+         PWM.setpwm(CurrentSettings.pwm)
+         print "Setting PWM to " + str(CurrentSettings.pwm) + " " + CurrentSettings.direction
+
+  return CurrentSettings
