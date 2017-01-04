08364b47a9f62cf63f0724ab5ef7a0fa69ee025c
Helric122915 <helric122915@yahoo.com>
2017-01-04 15:30:27 +0000

    Created new classes, 1 for each operation mode

--- FanClass.py
+++ FanClass.py
@@ -1,25 +1,46 @@
 class FanData(object):
 
-  def __init__(self,fanSpeed,roomTemp):
+  def __init__(self,opMode,direction,manualModeFanSpeed):
     # General Variables
-    self.roomTemp = roomTemp
-    self.fanOff = fanOff
+    self.opMode = opMode #Manual,OneTemp,TwoTemp,Schedule
     self.direction = direction
 
     # Manual Mode Variables
-    self.manualModeFanSpeed = fanSpeed
+    self.manualModeFanSpeed = manualModeFanSpeed
     
     # Schedule Mode Variables
     # What variables do we need to support this mode?
+    # I believe we will need a list of time intervals to support this mode
+    # The schedule mode will search through the list of intervals
+    # to check if the fan should currently be on and at which rate
+    # the fan should operate at.    
 
-    # One Source Temp Mode Variables
-    self.oneTempLowTemp = oneTempLowTemp
-    self.oneTempLowSpeed = oneTempLowSpeed
-    self.oneTempHighTemp = oneTempHighTemp
-    self.oneTempHighSpeed = oneTempHighSpeed
+class ManualData(object):
 
-    # Two Source Temp Mode Variables
-    self.twoTempLowTemp = twoTempLowTemp
-    self.twoTempLowSpeed = twoTempLowSpeed
-    self.twoTempHighTemp = twoTempHighTemp
-    self.twoTempHighSpeed = twoTempHighSpeed
+  def __init__(self,direction,pwm):
+    self.direction = direction
+    self.pwm = pwm
+
+class OneTempData(object):
+
+  def __init__(self,direction,lowSpeed,lowTemp,highSpeed,highTemp):
+    self.direction = direction
+    self.lowSpeed = lowSpeed
+    self.lowTemp = lowTemp
+    self.highSpeed = highSpeed
+    self.highTemp = highTemp
+
+class TwoTempData(object):
+
+  def __init__(self,lowSpeed,lowTemp,highSpeed,highTemp):
+    self.lowSpeed = lowSpeed
+    self.lowTemp = lowTemp
+    self.highSpeed = highSpeed
+    self.highTemp = highTemp
+
+class CurrentFanData(object):
+                      # just changed the order of pwm and direction will mess up calls to this class
+  def __init__(self,direction,pwm,power):
+    self.direction = direction
+    self.pwm = pwm
+    self.power = power
