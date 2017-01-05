653eedf5550f8bf6dc86ae2bbf6ef24c5965f840
Helric122915 <helric122915@yahoo.com>
2017-01-05 14:04:19 +0000

    Added slope and intercept calculations.

--- OneTempOperation.py
+++ OneTempOperation.py
@@ -1,6 +1,12 @@
 import FanClass
 import ReadSensor
+import PWM
 
-def AdjustPWM(FanData, CurrentPWM):
-
+def AdjustPWM(FanData, CurrentSettings):
   temp = ReadSensor.ReadTemp()
+
+  slope = (FanData.highSpeed - FanData.lowSpeed)/(FanData.highTemp - FanData.lowTemp)
+  intercept = (FanData.lowSpeed*FanData.highTemp - FanData.lowTemp*FanData.highSpeed)/(FanData.highTemp - FanData.lowTemp)
+
+  desiredPWM = slope * temp + intercept
+  
