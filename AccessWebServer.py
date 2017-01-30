c83115781bb343f4e290946f2444fa7d4d644683
Helric122915 <jbb47@zips.uakron.edu>
2017-01-30 17:03:17 -0500

    Adding address access and fixing PostoP

--- AccessWebServer.py
+++ AccessWebServer.py
@@ -1,32 +1,66 @@
 import requests
 import json
 import FanClass
+import time
 
 PORTNUM = '3000'
-url = 'http://130.101.94.161:' + PORTNUM
+url = 'http://130.101.95.132:' + PORTNUM
 
 def GetOp():
-  r = requests.get(url + '/GetOp')
+  try:
+    r = requests.get(url + '/GetOp')
 
-  data = json.loads(r.text)
-  opMode = data['data']['Mode']
+    if r.status_code != 200:
+      print "Error:", r_status_code
+      return "NoMode"
+    else:
+      data = json.loads(r.text)
+      opMode = data['data']['Mode']
 
-  return opMode
+      return opMode
+  
+  except:
+    return "NoMode";
 
 def PostOp(OpData):
   data = {}
-  data["data"] = OpData
+  data["Mode"] = OpData
 
   r = requests.post(url + '/PostOp', data)
   print r.text
 
+def GetAddress():
+  try:
+    r = requests.get(url + '/GetAddress')
+
+    if r.status_code != 200:
+      print "Error:", r.status_code
+      return FanClass.LocationData(0,0)
+    else:
+      data = json.loads(r.text)
+      locationData = FanClass.LocationData(data['data']['Latitude'],data['data']['Longitude'])
+
+      return locationData
+
+  except:
+    return FanClass.LocationData(0,0)
+    
+
 def GetManual():
-  r = requests.get(url + '/GetManual')
+  try:
+    r = requests.get(url + '/GetManual')
 
-  data = json.loads(r.text)
-  manualData = FanClass.ManualData(data['data']['Manual_Direction'],int(data['data']['Manual_Fan_Speed']))
+    if r.status_code != 200:
+      print "Error:", r.status_code
+      return FanClass.ManualData('N/A',-1)
+    else:
+      data = json.loads(r.text)
+      manualData = FanClass.ManualData(data['data']['Manual_Direction'],int(data['data']['Manual_Fan_Speed']))
 
-  return manualData
+      return manualData
+
+  except:
+    return FanClass.ManualData('N/A',-1)
 
 def PostManual(manualData):
   data = {}
@@ -36,13 +70,37 @@ def PostManual(manualData):
   r = requests.post(url + '/PostManual', data)
   print r.text
 
-def GetOneTemp():
-  r = requests.get(url + '/GetOneTemp')
+def GetCurrentSchedule():
+  try:
+    r = requests.get(url + '/GetCurrentSchedule')
 
-  data = json.loads(r.text)
-  oneTempData = FanClass.OneTempData(data['data']['One_Temp_Direction'],int(data['data']['One_Temp_Low_Speed']),int(data['data']['One_Temp_Low_Temp']),int(data['data']['One_Temp_High_Speed']),int(data['data']['One_Temp_High_Temp']))
+    if r.status_code != 200:
+      print "Error:", r.status_code
+      return FanClass.ScheduleData('N/A',-1)
+    else:
+      data = json.loads(r.text)
+      scheduleData = FanClass.ScheduleData(data['data']['Direction'],data['data']['Fan_Speed'])
+    
+      return scheduleData
   
-  return oneTempData
+  except:  
+    return FanClass.ScheduleData('N/A',-1)
+
+def GetOneTemp():
+  try:
+    r = requests.get(url + '/GetOneTemp')
+
+    if r.status_code != 200:
+      print "Error:", r.status_code
+      return FanClass.OneTempData('N/A',0,0,0,0)
+    else:
+      data = json.loads(r.text)
+      oneTempData = FanClass.OneTempData(data['data']['One_Temp_Direction'],int(data['data']['One_Temp_Low_Speed']),int(data['data']['One_Temp_Low_Temp']),int(data['data']['One_Temp_High_Speed']),int(data['data']['One_Temp_High_Temp']))
+  
+      return oneTempData
+
+  except:
+    return FanClass.OneTempData('N/A',0,0,0,0)
 
 def PostOneTemp(oneTempData):
   data = {}
@@ -56,12 +114,20 @@ def PostOneTemp(oneTempData):
   print r.text
 
 def GetTwoTemp():
-  r = requests.get(url + '/GetTwoTemp')
+  try:
+    r = requests.get(url + '/GetTwoTemp')
 
-  data = json.loads(r.text)
-  twoTempData = FanClass.TwoTempData(int(data['data']['Two_Temp_Low_Speed']),int(data['data']['Two_Temp_Low_Temp']),int(data['data']['Two_Temp_High_Speed']),int(data['data']['Two_Temp_High_Temp']))
+    if r.status_code != 200:
+      print "Error:", r.status_code
+      return FanClass.TwoTempData(0,0,0,0)
+    else:
+      data = json.loads(r.text)
+      twoTempData = FanClass.TwoTempData(int(data['data']['Two_Temp_Low_Speed']),int(data['data']['Two_Temp_Low_Temp']),int(data['data']['Two_Temp_High_Speed']),int(data['data']['Two_Temp_High_Temp']))
 
-  return twoTempData
+      return twoTempData
+
+  except:
+    return FanClass.TwoTempData(0,0,0,0)
 
 def PostTwoTemp(twoTempData):
   data = {}
