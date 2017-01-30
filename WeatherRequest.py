635e2a05ae631700b5af945e64a38b5a8553877e
Helric122915 <jbb47@zips.uakron.edu>
2017-01-30 17:11:05 -0500

    Reconfiguring weather access to cache for 5 minutes

--- WeatherRequest.py
+++ WeatherRequest.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 import LocationRequest
+import AccessWebServer
+import FanClass
 
 api_url = "https://api.darksky.net/forecast/%s/%f,%f"
 api_key = "9c1f7351528fc58f40881d5bf2651719"
@@ -18,22 +20,37 @@ def CurrentTempAddress(address):
   temp = CurrentTemp(location.lat, location.lng)
   return temp
 
-def Current(lat = 41.07472, lng = -81.52201):
-  query_url = api_url % (api_key, int(float(lat)), int(float(lng)))
-  r = requests.get(query_url)
+def Current():
+  locationData = AccessWebServer.GetAddress()
+
+  if locationData.latitude == 0 or locationData.longitude == 0:
+    locationData.latitude = 41.076029
+    locationData.longitude = -81.502305
+ 
+  query_url = api_url % (api_key, locationData.latitude, locationData.longitude)
+  r = requests.get(query_url);
 
   if r.status_code != 200:
     print "Error:", r.status_code
   else:
     json_weather = r.json()
-    print json_weather
+    #print "API Query"
     return json_weather
 
-def CurrentTemp(lat = 41.07472, long = -81.52201):
-  json_weather = Current(lat, long)
+weatherData = FanClass.WeatherData(Current(), time.time())
 
-  return json_weather['currently']['temperature']
+def WeatherData():
+  global weatherData
+  if weatherData.ElapsedTime() > 300:
+    weatherData = FanClass.WeatherData(Current(), time.time())
+  
+  return weatherData.json
 
+def CurrentTemp():
+  json_weather = WeatherData()
+
+  # Temporarily adding 55.0 to make the temperature more appropriate.
+  return float(json_weather['currently']['temperature']) + 55.0
 
 #print json_weather
 #print "\n"
