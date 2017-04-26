8f1341b9852e560eddb9178ad53cebf06eb4a928
Helric122915 <jbb47@zips.uakron.edu>
2017-04-25 22:39:44 -0400

    Configured Ethernet

--- Internet.py
+++ Internet.py
@@ -2,7 +2,7 @@ from subprocess import *
 from datetime import datetime
 
 cmdEth0 = "ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
-cmdWlan0 = "ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+#cmdWlan0 = "ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
 
 def run_cmd(cmd):
   p = Popen(cmd, shell=True, stdout=PIPE)
@@ -10,9 +10,9 @@ def run_cmd(cmd):
   return output
 
 def getIP():
-  if run_cmd(cmdWlan0) != "":
-    return run_cmd(cmdWlan0)
-  elif run_cmd(cmdEth0) != "":
+  #if run_cmd(cmdWlan0) != "":
+  #  return run_cmd(cmdWlan0)
+  if run_cmd(cmdEth0) != "":
     return run_cmd(cmdEth0)
   else:
     return "No IP Address Found."
